dd77e00d88421f68a78485c19d7c99c7811e024e
Suemayah <suemayah.eldursi@gmail.com>
2024-02-08 09:45:54 +0000

    add some ideas

--- intermediate demo code/visualise.py
+++ intermediate demo code/visualise.py
@@ -1,1 +1,5 @@
 # see the visualissation of the latents in https://github.com/fastai/diffusion-nbs/blob/master/Stable%20Diffusion%20Deep%20Dive.ipynb
+# this code here has a lot of images which can be used: https://colab.research.google.com/github/huggingface/diffusion-models-class/blob/main/unit2/01_finetuning_and_guidance.ipynb#scrollTo=U1OkUj0iiLaW
+# also check it to see if I can use the mthod of guidance during inference for layouts
+# show text tokens here https://colab.research.google.com/github/sagiodev/stablediffusion_webui/blob/master/Stable_Diffusion_tokenizer_and_embedding_SDA.ipynb
+# How to visualise the neural network architecture: https://towardsdatascience.com/how-to-visualize-neural-network-architectures-in-python-567cd2aa6d62
